e796d7164c8b9d8509294ddc93e9e38e7a514f0e
susanX <susan.gooding@yahoo.co.uk>
2015-09-07 09:13:14 +0100

    Delete maze-solver readme.pl

--- prolog/maze-solver readme.pl
+++
@@ -1,5 +0,0 @@
-reference adapted from: http://www.i-programmer.info/programming/artificial-intelligence/1331-database-the-prolog-way.html?start=2 
-
-This is a work in progress not finished.
-Parts of it were worked on with Charlene Furguson.
-Other general reference: Prolog Programming for Students, Callear, D. 1994 Hants
